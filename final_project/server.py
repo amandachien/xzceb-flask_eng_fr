ea1011203a00c6e3b0cc6712ceac53193820a54d
amandachien <amandacollaborative@gmail.com>
2022-11-04 23:26:04 -0400

    Final changes

--- final_project/server.py
+++ final_project/server.py
@@ -26,8 +26,8 @@ def french_to_english():
     """
     text_to_translate = request.args.get('text_to_translate')
     # Write your code here
-    english_text= translator.french_to_english(text_to_translate)
-    return english_text
+    french_text= translator.french_to_english(text_to_translate)
+    return french_text
 
 @app.route("/")
 def render_index_page():
